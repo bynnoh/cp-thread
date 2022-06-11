8c86a6c5be4b64a35b644cdc7057a26129c78640
nbmyng <nbmyng@gmail.com>
2022-06-11 20:24:05 +0900

    feat: CBV Thread, Comment List/Detail/CreateView

--- main/models.py
+++ main/models.py
@@ -1,11 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.urls import reverse
-
-# Create your models here.
 
 class Thread(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=5000)
+    author = models.ForeignKey(User, null=False)
 
     likes = models.IntegerField(default=0, editable=False)
     down = models.IntegerField(default=0, editable=False)
@@ -14,7 +14,7 @@ class Thread(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return reverse("thread_detail", kwargs={"pk": self.pk})
@@ -22,6 +22,7 @@ class Thread(models.Model):
 
 class Comment(models.Model):
     content = models.TextField(max_length=1000)
+    author = models.ForeignKey(User, null=False)
 
     likes = models.IntegerField(default=0, editable=False)
     down = models.IntegerField(default=0, editable=False)
@@ -30,7 +31,7 @@ class Comment(models.Model):
     updated_at = models.DateTImeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return f'{self.author} | {self.content}'
 
     def get_absolute_url(self):
         return f'{self.thread.get_absolute_url()}#{self.pk}'
--- main/views.py
+++ main/views.py
@@ -1,9 +1,27 @@
-from django.views.generic import ListView
+from typing import List
+from xml.etree.ElementTree import Comment
+from django.views.generic import ListView, DetailView, CreateView
 from django.shortcuts import render
 
-from .models import Thread
+from .models import Thread, Comment
 
 # Create your views here.
 
 class ThreadList(ListView):
     model = Thread
+
+class ThreadDetai(DetailView):
+    model = Thread
+
+class ThreadCreate(CreateView):
+    model = Thread
+    fields = ['title', 'content']
+
+class CommentList(ListView):
+    model = Comment
+
+class CommentDetail(DetailView):
+    model = Comment
+
+class CommentCreate(CreateView):
+    model = Comment
