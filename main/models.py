1e73f043b9a87111b0af9a5f3e38b46fa90e5f40
nbmyng <nbmyng@gmail.com>
2022-06-11 20:11:53 +0900

    feat: comment model, url method

--- main/models.py
+++ main/models.py
@@ -1,11 +1,37 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
 class Thread(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=5000)
+
     likes = models.IntegerField(default=0, editable=False)
+    down = models.IntegerField(default=0, editable=False)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("thread_detail", kwargs={"pk": self.pk})
+    
+
+class Comment(models.Model):
+    content = models.TextField(max_length=1000)
+
+    likes = models.IntegerField(default=0, editable=False)
+    down = models.IntegerField(default=0, editable=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTImeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'{self.thread.get_absolute_url()}#{self.pk}'
+    
