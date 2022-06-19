37cc8dd63997e368c60c42ae451d279ec05cbe22
nbmyng <nbmyng@gmail.com>
2022-06-19 23:08:26 +0900

    feat: bootstrap / submit comment with crispy form

--- main/forms.py
+++ main/forms.py
@@ -1,8 +1,27 @@
 
-from django import forms
+from django.forms import ModelForm, Textarea
 from .models import Comment
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit
+from crispy_forms.bootstrap import FormActions, FieldWithButtons, StrictButton
 
-class CommentForm(forms.ModelForm):
+class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+        labels = {
+            'content': ''
+        }
+        widgets = {
+            'content': Textarea(attrs={'cols': 10, 'rows': 1}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            FieldWithButtons(
+                'content',
+                StrictButton("댓글 작성", type="submit", css_class="btn-primary")
+            ),
+        )
