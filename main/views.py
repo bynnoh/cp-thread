a8c85fecbb5f3b1bb2cb50b8476814e102785c48
nbmyng <nbmyng@gmail.com>
2022-06-20 19:48:36 +0900

    feat: submit button in thread list

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView, CreateView
 from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Topic, Thread, Comment, Vote
 from .forms import ThreadForm, CommentForm
 
@@ -12,9 +13,20 @@ class ThreadList(ListView):
 # class ThreadDetail(DetailView):
 #     model = Thread
 
-class ThreadCreate(CreateView):
+class ThreadCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Thread
     form_class = ThreadForm
+
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
+    def form_valid(self, form):
+        current_user = self.request.user
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
+            form.instance.author = current_user
+            return super(ThreadCreate, self).form_valid(form)
+        else:
+            return redirect('/blog/')
 
     # def get_context_data(self, **kwargs):
     #     context = super(ThreadCreate, self).get_context_data(**kwargs)
