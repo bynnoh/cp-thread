245f9e747d1ed6923d913cb2c38e31be82b54f71
nbmyng <nbmyng@gmail.com>
2022-06-20 21:56:41 +0900

    style: comment img order

--- main/forms.py
+++ main/forms.py
@@ -2,13 +2,16 @@
 from django.forms import ModelForm, Textarea
 from .models import Thread, Comment
 from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Layout
+from crispy_forms.layout import Layout, Field
 from crispy_forms.bootstrap import FieldWithButtons, StrictButton
 
 class ThreadForm(ModelForm):
     class Meta:
         model = Thread
         fields = ['title', 'content', 'image', 'topic']
+        widgets = {
+            'content': Textarea(attrs={'cols': 12, 'rows': 3}),
+        }
 
 class CommentForm(ModelForm):
     class Meta:
@@ -18,17 +21,15 @@ class CommentForm(ModelForm):
             'content': '댓글',
             'image': '댓글 이미지 첨부'
         }
-        widgets = {
-            'content': Textarea(attrs={'cols': 10, 'rows': 5}),
-        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.helper = FormHelper()
+        self.fields['content'].widget.attrs = {'cols': 100, 'rows': 3}
         self.helper.layout = Layout(
             FieldWithButtons(
                 'content',
-                'image',
                 StrictButton("댓글 작성", type="submit", css_class="btn-primary"),
             ),
+            Field('image')
         )
