ae81a8bb78921957915287b556bb87438addfb1b
nbmyng <nbmyng@gmail.com>
2022-06-20 22:27:13 +0900

    fix: cf staging

--- thread_prj/settings.py
+++ thread_prj/settings.py
@@ -142,7 +142,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # Media files
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = 'media'
 
 
 # Default primary key field type
