fb48aedb68d2c7542fe5095bc24d177381a3c200
nbmyng <nbmyng@gmail.com>
2022-06-28 19:07:47 +0900

    style: code cleanup

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,8 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.main_thread_list),
+    # path('', views.main_thread_list),
+    path('', views.ThreadList.as_view()),
     path('<int:pk>', views.thread_detail, name='thread-detail'),
     path('<int:pk>/update', views.ThreadUpdate.as_view(), name='thread-update'),
     path('<int:pk>/submit-comment', views.submit_comment, name='submit-comment'),
