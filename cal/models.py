a972fa9d6f58e02999a94a08293471f6b6593366
nbmyng <nbmyng@gmail.com>
2022-07-15 23:30:45 +0900

    feat: platform model & referenced by event

--- cal/models.py
+++ cal/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 from markdownx.models import MarkdownxField
 
+class Platform(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'{self.name}'
+
 class Event(models.Model):
-    event = MarkdownxField(max_length=50)
-    desc = MarkdownxField(max_length=5000)
+    event = models.CharField(max_length=50)
     start = models.DateTimeField()
     end = models.DateTimeField()
+    platform = models.ForeignKey(Platform, on_delete=models.SET_NULL)
+    desc = MarkdownxField()
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
